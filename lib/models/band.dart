28ac6b18e90c09bebde670b542501c7fc8322a2b
Santiago Gonzalez Fernandez <santiagogonzalezfernandezpro@gmail.com>
2020-11-30 12:33:42 -0300

    Fin seccion 2

---
+++ lib/models/band.dart
@@ -0,0 +1,22 @@
+//Imports that are not mine
+
+//Imports that are mine
+
+class Band{
+
+  String id;
+  String name;
+  int votes;
+
+  Band({
+    this.id,
+    this.name,
+    this.votes
+  });
+
+  factory Band.fromMap(Map<String, dynamic> object) => Band(
+    id: object['id'],
+    name: object['name'],
+    votes: object['votes'],
+  );
+}
